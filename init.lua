022c19d9ae52824552555020796a80db2dd6aef6
Hugo Burton <hugo.burton@westpac.com.au>
2025-10-05 03:01:58 +1100

    feat: add extra essential plugins

--- init.lua
+++ init.lua
@@ -73,6 +73,46 @@ require("lazy").setup({
       },
     },
   },
+
+  -- Extra essentials
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("which-key").setup({})
+    end,
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({})
+    end,
+  },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup({})
+    end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("trouble").setup({})
+    end,
+  },
 })
 
 -- =========================
