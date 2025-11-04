9db633e8a3182e408371e0c5b22481a0f69e8de9
Freya Sambain <freya.sambain@westpac.com.au>
2025-11-04 16:04:40 +1000

    fix: turn off relative numbering

--- lua/freya/core/options.lua
+++ lua/freya/core/options.lua
@@ -2,7 +2,7 @@
 -- General settings
 -- =========================
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
