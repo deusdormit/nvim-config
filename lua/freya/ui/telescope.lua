8daa8ffddc6965a119a0829d84a55ba5a0271382
Freya Sambain <freya.sambain@westpac.com.au>
2025-11-06 10:41:26 +1000

    Update telescope config

--- lua/freya/ui/telescope.lua
+++ lua/freya/ui/telescope.lua
@@ -2,7 +2,11 @@
 -- Telescope keymaps
 -- =========================
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+  hidden = false,
+  no_ignore = true,
+  file_ignore_patterns = { ".git/", "node_modules", "dist", "build" },
+})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
