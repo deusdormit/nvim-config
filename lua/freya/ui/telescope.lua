21b05db5f8ffdf64f6290eb4e02ecf2f03d61710
deusdormit <104626678+deusdormit@users.noreply.github.com>
2025-11-06 12:11:14 +1000

    git credentials update test

--- lua/freya/ui/telescope.lua
+++ lua/freya/ui/telescope.lua
@@ -5,7 +5,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {
   hidden = false,
   no_ignore = true,
-  file_ignore_patterns = { ".git/", "node_modules", "dist", "build" },
+  file_ignore_patterns = { ".git/", "node_modules", "dist", "build", "tmp" },
 })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
